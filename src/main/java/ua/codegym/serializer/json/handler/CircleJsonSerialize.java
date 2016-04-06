ca9ee9449e340cdfe7dba76e5718892dadc7b34d
skuhtin <ceperazhv@gmail.com>
2016-04-07 00:10:44 +0300

    With added Circle and Group Json

---
+++ src/main/java/ua/codegym/serializer/json/handler/CircleJsonSerialize.java
@@ -0,0 +1,21 @@
+package ua.codegym.serializer.json.handler;
+
+
+import ua.codegym.serializer.Serializer;
+import ua.codegym.serializer.shape.Circle;
+import ua.codegym.serializer.shape.Shape;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class CircleJsonSerialize implements Serializer{
+  @Override
+  public void serialize(Shape shape, OutputStream os) throws IOException {
+    Circle circle = (Circle) shape;
+    int x = circle.getX();
+    int y = circle.getY();
+    int radius = circle.getRadius();
+    String out = String.format("{\"nameOfShape\": \"Circle\",\"properties\": {\"x\": %d,\"y\": %d,\"radius\": %d}}",x,y,radius);
+    os.write(out.getBytes());
+  }
+}
